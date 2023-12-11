0cd51ae43581ec79e7ec58079271a1e0b223e68f
roy0428 <98080372+roy0428@users.noreply.github.com>
2023-12-11 12:38:33 +0800

    Add files via upload

--- backend/views.py
+++ backend/views.py
@@ -13,50 +13,50 @@ import os
 import base64
 
 
-# def original(photo):
-#     image = Image.open(photo)
-#     np_photo = np.array(image)
-#     np_photo = np.rot90(np_photo, k=3)
-#     np_photo = cv2.cvtColor(np_photo, cv2.COLOR_RGB2BGR)
-#     return np_photo
+def original(photo):
+    image = Image.open(photo)
+    np_photo = np.array(image)
+    np_photo = np.rot90(np_photo, k=3)
+    np_photo = cv2.cvtColor(np_photo, cv2.COLOR_RGB2BGR)
+    return np_photo
 
 
-# def mask(ortho_image):
-#     return functions.inference.main(ortho_image)
+def mask(ortho_image):
+    return functions.inference.main(ortho_image)
 
 
-# @csrf_exempt
-# def upload_photo(request):
-#     if request.method == "POST" and request.FILES["photo"]:
-#         start_time = time.time()
-#         photo = request.FILES["photo"]
-#         original_image = original(photo)
-#         masked_image = mask(original_image)
-#         _, buffer = cv2.imencode(".jpeg", masked_image)
-#         end_time = time.time()
-#         print("Total Runtime:", end_time - start_time, "s")
-#         return HttpResponse(buffer.tobytes(), content_type="image/jpeg")
-#     else:
-#         return JsonResponse({"success": False})
+@csrf_exempt
+def upload_photo(request):
+    if request.method == "POST" and request.FILES["photo"]:
+        start_time = time.time()
+        photo = request.FILES["photo"]
+        original_image = original(photo)
+        masked_image = mask(original_image)
+        _, buffer = cv2.imencode(".jpeg", masked_image)
+        end_time = time.time()
+        print("Total Runtime:", end_time - start_time, "s")
+        return HttpResponse(buffer.tobytes(), content_type="image/jpeg")
+    else:
+        return JsonResponse({"success": False})
 
 
-# @csrf_exempt
-# def upload_video(request):
-#     if request.method == "POST" and request.FILES["video"]:
-#         start_time = time.time()
-#         video = request.FILES["video"]
-#         video_bytes = video.read()
-#         temp_path = os.path.join("backend/results", "temp_video.mp4")
-#         with open(temp_path, "wb") as f:
-#             f.write(video_bytes)
-#         video_capture = cv2.VideoCapture(temp_path)
-#         image = test_combine.main(video_capture)
-#         _, buffer = cv2.imencode(".jpeg", image)
-#         end_time = time.time()
-#         print("Total Runtime:", end_time - start_time, "s")
-#         return HttpResponse(buffer.tobytes(), content_type="image/jpeg")
-#     else:
-#         return JsonResponse({"success": False})
+@csrf_exempt
+def upload_video(request):
+    if request.method == "POST" and request.FILES["video"]:
+        start_time = time.time()
+        video = request.FILES["video"]
+        video_bytes = video.read()
+        temp_path = os.path.join("backend/results", "temp_video.mp4")
+        with open(temp_path, "wb") as f:
+            f.write(video_bytes)
+        video_capture = cv2.VideoCapture(temp_path)
+        image = test_combine.main(video_capture)
+        _, buffer = cv2.imencode(".jpeg", image)
+        end_time = time.time()
+        print("Total Runtime:", end_time - start_time, "s")
+        return HttpResponse(buffer.tobytes(), content_type="image/jpeg")
+    else:
+        return JsonResponse({"success": False})
 
 
 @csrf_exempt
@@ -103,26 +103,25 @@ def upload_ID(request):
 def upload_photo_for_seminar(request):
     if request.method == "POST" and request.FILES["photo"]:
         photo = request.FILES["photo"]
-        # TODO
-        # preprocessed_photo = blur(photo)
-        # description = gpt(preprocessed_photo)
-        description = "returned description"
+        only_for_seminar.data_protection(photo)
+        description = only_for_seminar.get_description()
         response_data = {"description": description}
         return JsonResponse(response_data)
     else:
         return JsonResponse({"success": False})
-    
+
+
 @csrf_exempt
 def update(request):
     if request.method == "POST":
         data = json.loads(request.body)
-        image = data.get("Image")
+        # image = data.get("Image")
         lat = data.get("Latitude")
         lng = data.get("Longitude")
         des = data.get("Description")
         id = data.get("UserID")
-        only_for_seminar.write_csv(image, lat, lng, des, id)
-        
+        only_for_seminar.write_csv(lat, lng, des, id)
+
         return JsonResponse({"success": True})
     else:
         return JsonResponse({"success": False})
